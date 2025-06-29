2a3f9bdd2a554b2116120339f43afbdfda99ed04
Yutong Sun <ysun730@gatech.edu>
2025-06-29 12:40:16 +0800

    Update stone animation.

--- Assets/Scripts/Objects/Interactables/Stone.cs
+++ Assets/Scripts/Objects/Interactables/Stone.cs
@@ -6,12 +6,25 @@ namespace Objects.Interactables
 {
     [RequireComponent(typeof(BoxCollider2D))]
     [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Animator))]
     public class Stone : Interactable
     {
+        private Animator animator;
+        
         protected override void Start()
         {
             base.Start();
             EventCenter.AddListener<bool>(EventType.PossessStone, SetSoul);
+            animator = GetComponent<Animator>();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (rb.linearVelocity.magnitude < 0.1f)
+                animator.SetBool("isRolling", false);
+            else
+                animator.SetBool("isRolling", true);
         }
 
         private void OnDisable()
